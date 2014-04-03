a67662aa299ba03b309d7466bf899af7ceee7986
ladyada <limor@ladyada.net>
2014-04-03 14:09:06 -0400

    init

---
+++ Adafruit_MCP9808.cpp
@@ -0,0 +1,106 @@
+/**************************************************************************/
+/*! 
+    @file     Adafruit_MCP9808.cpp
+    @author   K.Townsend (Adafruit Industries)
+	@license  BSD (see license.txt)
+	
+	I2C Driver for Microchip's MCP9808 I2C Temp sensor
+
+	This is a library for the Adafruit MCP9808 breakout
+	----> http://www.adafruit.com/products/1782
+		
+	Adafruit invests time and resources providing this open source code, 
+	please support Adafruit and open-source hardware by purchasing 
+	products from Adafruit!
+
+	@section  HISTORY
+
+    v1.0 - First release
+*/
+/**************************************************************************/
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+#include "Adafruit_MCP9808.h"
+
+/**************************************************************************/
+/*! 
+    @brief  Instantiates a new MCP9808 class
+*/
+/**************************************************************************/
+Adafruit_MCP9808::Adafruit_MCP9808() {
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Setups the HW
+*/
+/**************************************************************************/
+boolean Adafruit_MCP9808::begin(uint8_t addr) {
+  _i2caddr = addr;
+  Wire.begin();
+
+  if (read16(MCP9808_REG_MANUF_ID) != 0x0054) return false;
+  if (read16(MCP9808_REG_DEVICE_ID) != 0x0400) return false;
+
+  return true;
+}
+ 
+/**************************************************************************/
+/*! 
+    @brief  Sets the output voltage to a fraction of source vref.  (Value
+            can be 0..4095)
+
+    @param[in]  output
+                The 12-bit value representing the relationship between
+                the DAC's input voltage and its output voltage.
+    @param[in]  writeEEPROM
+                If this value is true, 'output' will also be written
+                to the MCP9808's internal non-volatile memory, meaning
+                that the DAC will retain the current voltage output
+                after power-down or reset.
+*/
+/**************************************************************************/
+float Adafruit_MCP9808::readTempC( void )
+{
+  uint16_t t = read16(MCP9808_REG_AMBIENT_TEMP);
+
+  float temp = t & 0x0FFF;
+  if (t & 0x1000) temp *= -1;
+  temp /=  16.0;
+
+  return temp;
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Low level 16 bit read and write procedures!
+*/
+/**************************************************************************/
+
+void Adafruit_MCP9808::write16(uint8_t reg, uint16_t value) {
+    Wire.beginTransmission(_i2caddr);
+    Wire.write((uint8_t)reg);
+    Wire.write(value >> 8);
+    Wire.write(value & 0xFF);
+    Wire.endTransmission();
+}
+
+uint16_t Adafruit_MCP9808::read16(uint8_t reg) {
+  uint16_t val;
+
+  Wire.beginTransmission(_i2caddr);
+  Wire.write((uint8_t)reg);
+  Wire.endTransmission();
+  
+  Wire.requestFrom((uint8_t)_i2caddr, (uint8_t)2);
+  val = Wire.read();
+  val <<= 8;
+  val |= Wire.read();  
+  return val;  
+}
