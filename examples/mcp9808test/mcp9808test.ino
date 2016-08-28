ffa194f3e64363a7efbbc36ae5ece254c6454fae
Daniel A. Fernandez R <dafrelectronics@outlook.com>
2016-08-27 21:35:43 -0500

    code fixed!!!

--- examples/mcp9808test/mcp9808test.ino
+++ examples/mcp9808test/mcp9808test.ino
@@ -39,19 +39,19 @@ void setup()
     Serial.println("Couldn't find MCP9808! Check your connections and verify the address is correct.");
     while (1); //don't do anything more.
   }
+
+  tempsensor.setResolution(3); // sets the resolution mode of reading, the modes are defined in the table bellow:
+  // Mode Resolution SampleTime
+  //  0    0.5°C       30 ms
+  //  1    0.25°C      65 ms
+  //  2    0.125°C     130 ms
+  //  3    0.0625°C    250 ms
 }
 
 void loop()
 {
   Serial.println("Wake up MCP9808.... "); // wake up MSP9808 - power consumption ~200 mikro Ampere
   tempsensor.shutdown_wake(0); // Don't remove this line! required before reading temp, it starts temperature sampling
-
-  tempsensor.setResolution(0); // sets the resolution mode of reading, the modes are defined in the table bellow:
-  // Mode Resolution SampleTime
-  //  0    0.5°C       30 ms
-  //  1    0.25°C      65 ms
-  //  2    0.125°C     130 ms
-  //  3    0.0625°C    250 ms
 
   // Read and print out the temperature, also shows the resolution mode used for reading.
   Serial.print("Resolution in mode: ");
@@ -61,10 +61,11 @@ void loop()
   Serial.print("Temp: ");
   Serial.print(c, 4); Serial.print("*C and ");
   Serial.print(f, 4); Serial.println("*F.");
-
-  Serial.println("Shutdown MCP9808.... ");
-  tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere, stops temperature sampling
   Serial.println("");
 
   delay(2000);
+  Serial.println("Shutdown MCP9808.... ");
+  tempsensor.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere, stops temperature sampling
+  Serial.println("");
+  delay(200);
 }
