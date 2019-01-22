41f2aa2258029daba417aff2d8679d9bd62263b6
Travis CI <travis@travis-ci.org>
2019-01-22 17:09:10 +0000

    Deploy code docs to GitHub Pages Travis build: 15
    Commit: f13dd7008ed245dcac94541fc778b9aa61f5ee79

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -3,6 +3,5 @@ var searchData=
   ['adafruit_5fmcp9808',['Adafruit_MCP9808',['../class_adafruit___m_c_p9808.html',1,'Adafruit_MCP9808'],['../class_adafruit___m_c_p9808.html#ac5b0197b1a6d693e612f134452041001',1,'Adafruit_MCP9808::Adafruit_MCP9808()']]],
   ['adafruit_5fmcp9808_2ecpp',['Adafruit_MCP9808.cpp',['../_adafruit___m_c_p9808_8cpp.html',1,'']]],
   ['adafruit_5fmcp9808_2eh',['Adafruit_MCP9808.h',['../_adafruit___m_c_p9808_8h.html',1,'']]],
-  ['adafruit_20mcp9808_20i2c_20temp_20sensor',['Adafruit MCP9808 I2C Temp Sensor',['../index.html',1,'']]],
-  ['adafruit_5fmcp9808_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fmcp9808_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fmcp9808_5flibrary_29',['Adafruit_MCP9808 [![Build Status](https://travis-ci.com/adafruit/Adafruit_MCP9808_Library.svg?branch=master)](https://travis-ci.com/adafruit/Adafruit_MCP9808_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20mcp9808_20i2c_20temp_20sensor',['Adafruit MCP9808 I2C Temp Sensor',['../index.html',1,'']]]
 ];
