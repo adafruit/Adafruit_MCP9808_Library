0e46dbe9999b3d91870e1861cab8f444bacb3c27
Doxygen CI <ci-arduino@invalid>
2021-05-10 14:19:08 +0000

    Deploy docs to GitHub Pages from commit 4f2b2d938f
    Commit: 4f2b2d938fe23705626923140f9a8f5a3a135458
    GitHub Actions run: 828321779

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['getevent',['getEvent',['../class_adafruit___m_c_p9808.html#ae1bc9235e96a5be86ae491b463c5e45c',1,'Adafruit_MCP9808']]],
-  ['getresolution',['getResolution',['../class_adafruit___m_c_p9808.html#ab561f281d6df2900a7dd3917c28367cb',1,'Adafruit_MCP9808']]],
-  ['getsensor',['getSensor',['../class_adafruit___m_c_p9808.html#a37bfe04aea9d0799607583efadcaf48b',1,'Adafruit_MCP9808']]]
+  ['getresolution',['getResolution',['../class_adafruit___m_c_p9808.html#ab561f281d6df2900a7dd3917c28367cb',1,'Adafruit_MCP9808']]]
 ];
